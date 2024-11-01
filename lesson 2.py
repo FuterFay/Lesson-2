631bb00e398f6ff9d7fa0702527624a1cce7072b
FuterFay <amolodcov11@gmail.com>
2024-11-01 20:40:57 +0300

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,5 @@
+homework =12
+hour = 1.5
+course ="Python"
+one_homework = 1.5/12
+print(f"Курс: {course},",f"всего задач:{homework},", f"затрачено часов: {hour},", "среднее время выполнения", one_homework,"часа.")
